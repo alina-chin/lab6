7003cbf30ff427ebf8ff376c35fe05d77ad50e95
Alina Chin <amc093@ucsd.edu>
2018-02-14 23:32:31 -0800

    ajax and facebook lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -27,4 +27,17 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+
+	$.get("project/" + idNumber, callback);
+	console.log("/project/" + idNumber);
+}
+
+function callback(result) {
+	console.log(result);
+	var projectHTML = '<img src="' + result.image +
+		'" class="detailsImage">' +
+    	'<p>' + result.title + '</p>' +
+    	'<p><small>' + result.date + '</small></p>' + 
+    	result.summary; 
+	$("#project" + result.id + " .details").html(projectHTML);
 }
